fc1cd0162483f4e3502268dfa3b2f0bf335b16de
Wesley Stroeber <wesley@geekcore.org>
2020-07-18 23:24:32 -0700

    Code changes and added template for README

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const questions = [
         type: "list",
         name: "license",
         message: 'What license would you like to use for this application?',
-        choices: ['MIT', 'GNU GPLv3', 'None']
+        choices: ['MIT', 'GNU GPLv3', 'ISC']
     },
     {
         type: 'input',
@@ -53,11 +53,52 @@ const questions = [
     }
 ];
 
-function init() {
+function promptUser() {
     inquirer.prompt(questions).then((answers) => {
         console.log(answers);
 
-        const response = ``
+        if (answers.license == 'MIT') {
+            answers.license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        } if (answers.license == 'GNU GPLv3') {
+            answers.license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+        } if (answers.license == 'ISC') {
+            answers.license = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+        }
+
+        const response = `
+# ${answers.title}
+
+${answers.license}
+
+## Description
+${answers.description}
+
+---
+## Table of Contents
+[Installation/Usage](#Installation/Usage-Instructions)  
+[Usage](#Usage-Information)  
+[Contributors](#Contributors)  
+[Testing](#Testing)  
+[Contact](#Contact)  
+
+---
+## Installation/Usage Instructions
+${answers.install}  
+${answers.usage}  
+
+## Contributors
+${answers.contributor}
+
+## Testing
+${answers.test}
+
+---
+## Contact
+
+Github: [${answers.github}](https://github.com/${answers.github})
+
+Email: <${answers.email}>
+        `
 
         fs.writeFile('README.md', response, (err) => {
             if (err) {
@@ -70,4 +111,4 @@ function init() {
     })
 }
 
-init();
+promptUser();
